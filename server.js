8787a7add9ea3b4bb0cb0d7fe292f911741b8ced
doan.dominh <doan.dominh@gameloft.com>
2019-02-25 11:43:34 +0700

    - update

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+var app = require('express')();
+var http = require('http').Server(app);
 var port = process.env.PORT || 3000;
-var io = require('socket.io')(port);
+var io = require('socket.io')(http);
 var shortId = require('shortid');
-
 var players = [];
 
 var playerSpeed = 3;
